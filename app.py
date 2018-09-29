7dc272493c6384fc03329f41d4f157b8ca345579
neelgeek <neelbhave91@gmail.com>
2018-09-29 16:49:47 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request,Response
+import jsonpickle
+import numpy as np
+import cv2
+app = Flask(__name__)
+
+@app.route("/getCount",methods=['POST'])
+def postImg():
+    r = request
+    
+    nparr = np.fromstring(r.data, np.uint8)
+
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+
+    response_pickled = jsonpickle.encode(response)
+
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route("/",methods=['GET'])
+def default():
+    return "Welcome !"
+if __name__ == '__main__':
+    print("App Started")
+    app.run(debug=True)
