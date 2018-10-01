f418f647939d1f8cfe2cb0185c73d5d71a3b472f
neelgeek <neelbhave91@gmail.com>
2018-10-01 09:47:49 +0530

    Fixed form data

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from flask import request,Response
 import jsonpickle
 import numpy as np
 import cv2
+import base64
+
 app = Flask(__name__)
 
 
 @app.route("/getCount",methods=['POST'])
 def postImg():
     r = request
-
-    nparr = np.fromstring(r.data, np.uint8)
-
+    img = r.files['image']   # Add file field name as 'image'
+    imgBin = img.read()
+    nparr = np.fromstring(imgBin, np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
