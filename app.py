e1245f180019529625d33934ca3ca58f3a7ced45
neelgeek <neelbhave91@gmail.com>
2018-10-01 21:56:30 +0530

    Added Logging

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import request,Response
 import jsonpickle
 import numpy as np
 import cv2
-import base64
+import logging
+from logging.handlers import RotatingFileHandler
+from time import strftime
 
 app = Flask(__name__)
 
@@ -27,7 +29,18 @@ def postImg():
 def default():
     return "Welcome !"
 
+@app.after_request
+def after_request(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    logger.error('%s || %s || %s || %s || %s || %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
+
+
 if __name__ == '__main__':
+    handler = RotatingFileHandler('app.log', maxBytes=100000, backupCount=3)
+    logger = logging.getLogger('tdm')
+    logger.setLevel(logging.ERROR)
+    logger.addHandler(handler)
     print("App Started")
-    app.run(debug=True)
+    app.run()
 
